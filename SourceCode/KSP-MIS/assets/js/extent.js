2a4fe91730cddac24d32e51089346799a36aca3c
rocketer <tsathit40@hotmail.com>
2019-12-19 21:10:14 +0700

    เพิ่ม Doc js และ highlight left aside

--- SourceCode/KSP-MIS/assets/js/extent.js
+++ SourceCode/KSP-MIS/assets/js/extent.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
             case "HR":
             case "BDG":
             case "ASS":
+            case "DOC":
                 break;
             default: return;
                 break;
